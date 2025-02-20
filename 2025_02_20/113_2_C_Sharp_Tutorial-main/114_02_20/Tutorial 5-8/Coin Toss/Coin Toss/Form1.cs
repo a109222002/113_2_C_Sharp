25df29d33d9ed68b6433700bffa0f5fd12aa82bc
a109222002 <a109222002@mail.shu.edu.tw>
2025-02-20 12:48:11 +0800

    2025_02_20

---
+++ 2025_02_20/113_2_C_Sharp_Tutorial-main/114_02_20/Tutorial 5-8/Coin Toss/Coin Toss/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coin_Toss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tossButton_Click(object sender, EventArgs e)
+        {
+            int sideUp;
+
+            Random raanf = new Random();
+            sideUp = Random.
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
