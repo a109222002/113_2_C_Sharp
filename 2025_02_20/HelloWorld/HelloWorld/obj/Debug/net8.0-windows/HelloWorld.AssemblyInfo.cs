427dac61e1266c8867af0ce66baaecf6552a6868
a109222002 <a109222002@mail.shu.edu.tw>
2025-03-06 13:02:32 +0800

    2025_03_06

--- 2025_02_20/HelloWorld/HelloWorld/obj/Debug/net8.0-windows/HelloWorld.AssemblyInfo.cs
+++ 2025_02_20/HelloWorld/HelloWorld/obj/Debug/net8.0-windows/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad63e41a492980ebe229914a6b12e5af411b2b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25df29d33d9ed68b6433700bffa0f5fd12aa82bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
